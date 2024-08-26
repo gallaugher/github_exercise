914d31af7ea9404a00b100cc54a4ea6c9b5a4fff
gallaugher <john.gallaugher@bc.edu>
2024-08-25 22:51:37 -0400

    Initial Commit

---
+++ github_exercise/ContentView.swift
@@ -0,0 +1,21 @@
+// File: ContentView.swift Project: github_exercise
+// Created by: Prof. John Gallaugher on 8/25/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ github_exercise/github_exerciseApp.swift
@@ -0,0 +1,14 @@
+// File: github_exerciseApp.swift Project: github_exercise
+// Created by: Prof. John Gallaugher on 8/25/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+@main
+struct github_exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
